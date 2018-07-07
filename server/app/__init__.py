ef9d7a8aa133d244f7ef2338db82caeb39a3a747
Ryan Frazier <pianomanfrazier@gmail.com>
2018-07-07 10:01:17 -0600

    add procfile and authentication

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from .authentication import requires_auth
 
 # TODO: serve static files using WhiteNoise
 app = Flask(__name__,
@@ -6,5 +7,6 @@ app = Flask(__name__,
             template_folder='./dist')
 
 @app.route('/')
+@requires_auth
 def index():
     return render_template('index.html') # this is a static file
